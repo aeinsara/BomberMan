6d45fbbd5b208f2f01131e68b4e4583a176d1fb4
saminfatehir <saminfatehir@gmail.com>
2016-06-25 14:06:26 +0430

    graphicl conection (client & sever)

--- Manager_server.cpp
+++ Manager_server.cpp
@@ -116,86 +116,19 @@ int getY(Wall **wall, Position * pos)
 		}
 		gui->close();
 	}
+	
 
+	std::string server_data = " ";
 
 	void Manager::handleGame()
 	{
 	
 	//.....................................* server *.....................................
 	
-	std::string serverData = " ";
-	std::string clientData ;
+	std::string client_data ;
 			
 	ServerSocket new_sock;
 	
-	//.................................* end of server *......................................
-	
-		Position *pos = world->getupMan()->getPosition();
-		
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-			{
-				bool a = interface(world->getWall(), world->getupMan()->getPosition(), world->getAry());
-				if(a == true)
-					pos->y+=5;
-					
-				serverData = "Down";	
-					
-				world->getupMan()->setFace("front");
-			}
-			
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-			{
-				bool a2 = interface2(world->getWall(), world->getupMan()->getPosition(), world->getAry());
-				if(a2 == true)
-					pos->y-=5;
-					
-				serverData = "Up";
-				
-				world->getupMan()->setFace("back");
-			}
-			
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-			{
-				bool a3 = interface3(world->getWall(), world->getupMan()->getPosition(), world->getAry());	
-				if(a3 == true)				
-					pos->x+=5;
-					
-				serverData = "Right";
-				
-				world->getupMan()->setFace("right");				
-			}
-			
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-			{
-				bool a4 = interface4(world->getWall(), world->getupMan()->getPosition(), world->getAry());
-				if(a4 == true)
-					pos->x-=5;
-				
-				serverData = "Left";
-				
-				world->getupMan()->setFace("left");	
-				//int i = getX(world->getWall(), world->getupMan()->getPosition());
-				//int j = getY(world->getWall(), world->getupMan()->getPosition());
-				//cout<<"i  "<<i<<"  j   "<<j<<endl;	
-			}
-			
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-			{
-				world->getupBomb()->setPosition(pos->x, pos->y);
-				int i = getX(world->getWall(), world->getupMan()->getPosition());
-				int j = getY(world->getWall(), world->getupMan()->getPosition());
-				world->getupBomb()->Explosion(world->getAry(), i, j);
-				cout<<pos->x<<"   "<<pos->y<<endl;
-				cout<<"i  "<<i<<"  j   "<<j<<endl;
-				
-				serverData = "Bomb";	
-			}
-			
-		world->getupMan()->setPosition(pos->x, pos->y);
-		
-		
-	//.....................................* server *.....................................
-
 	std::cout << "running....\n";
 
   try
@@ -208,8 +141,8 @@ int getY(Wall **wall, Position * pos)
 
 	  try
 	    {
-		  new_sock >> clientData;
-		  new_sock << serverData;
+		  new_sock >> client_data;
+		  new_sock << server_data;
 	    }
 	  catch ( SocketException& ) {}
 
@@ -220,7 +153,176 @@ int getY(Wall **wall, Position * pos)
       std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
     }
 	
-	std::cout << clientData << "\n";
+	std::cout << client_data << "\n";
+		
+	//.................................* end of server *......................................
+	
+		Position *posUp = world->getupMan()->getPosition();
+		Position *posDown = world->getdownMan()->getPosition();
+		
+//............................................* Up Man *...........................................
+    
+    if(client_data == "Down")
+    {
+      bool a = interface(world->getWall(), world->getupMan()->getPosition(), world->getAry());
+      if(a == true)
+        posUp->y+=5;
+        
+		world->getupMan()->setFace("front");
+        
+
+    }
+    
+    if(client_data == "Up")
+    {
+      bool a2 = interface2(world->getWall(), world->getupMan()->getPosition(), world->getAry());
+      if(a2 == true)
+        posUp->y-=5;
+        
+ 		world->getupMan()->setFace("back");
+       
+        
+    }
+    
+    if(client_data == "Right")
+    {
+      bool a3 = interface3(world->getWall(), world->getupMan()->getPosition(), world->getAry());  
+      if(a3 == true)        
+        posUp->x+=5;  
+        
+		world->getupMan()->setFace("right");
+                
+    }
+    
+    if(client_data == "Left")
+    {
+      bool a4 = interface4(world->getWall(), world->getupMan()->getPosition(), world->getAry());
+      if(a4 == true)
+        posUp->x-=5;
+          
+		world->getupMan()->setFace("left");
+          
+    }
+    
+        
+    if(client_data == "Bomb")
+    {
+      world->getupBomb()->setPosition(posUp->x, posUp->y);
+      int i = getX(world->getWall(), world->getupMan()->getPosition());
+      int j = getY(world->getWall(), world->getupMan()->getPosition());
+      world->getupBomb()->Explosion(world->getAry(), i, j);
+ //     std::cout « posUp->x « "   " « posUp->y « endl;
+   //   std::cout « "i  " « i « "  j   " « j « endl;
+        
+   //   server_data = "Bomb";  
+    }
+      
+      
+    world->getupMan()->setPosition(posUp->x, posUp->y);		
+		
+		
+		
+		
+	// ................................* Down man (server) *...................................
+		
+		
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+			{
+				bool a = interface(world->getWall(), world->getdownMan()->getPosition(), world->getAry());
+				if(a == true)
+					posDown->y+=5;
+					
+				server_data = "Down";	
+					
+				world->getdownMan()->setFace("front");
+			}
+			
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+			{
+				bool a2 = interface2(world->getWall(), world->getdownMan()->getPosition(), world->getAry());
+				if(a2 == true)
+					posDown->y-=5;
+					
+				server_data = "Up";
+				
+				world->getdownMan()->setFace("back");
+			}
+			
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+			{
+				bool a3 = interface3(world->getWall(), world->getdownMan()->getPosition(), world->getAry());	
+				if(a3 == true)				
+					posDown->x+=5;
+					
+				server_data = "Right";
+				
+				world->getdownMan()->setFace("right");				
+			}
+			
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+			{
+				bool a4 = interface4(world->getWall(), world->getdownMan()->getPosition(), world->getAry());
+				if(a4 == true)
+					posDown->x-=5;
+				
+				server_data = "Left";
+				
+				world->getdownMan()->setFace("left");	
+				//int i = getX(world->getWall(), world->getupMan()->getPosition());
+				//int j = getY(world->getWall(), world->getupMan()->getPosition());
+				//cout<<"i  "<<i<<"  j   "<<j<<endl;	
+			}
+			
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+			{
+				world->getupBomb()->setPosition(posDown->x, posDown->y);
+				int i = getX(world->getWall(), world->getdownMan()->getPosition());
+				int j = getY(world->getWall(), world->getdownMan()->getPosition());
+				world->getupBomb()->Explosion(world->getAry(), i, j);
+	//			cout<<posDown->x<<"   "<<posDown->y<<endl;
+		//		cout<<"i  "<<i<<"  j   "<<j<<endl;
+				
+				server_data = "Bomb";	
+			}
+			
+			else
+			{
+				server_data = " ";
+			}
+			
+		world->getdownMan()->setPosition(posDown->x, posDown->y);		
+		
+	//.....................................* server *.....................................
+
+	std::cout << "running....\n";
+
+  try
+    {
+      // Create the socket
+ //     ServerSocket server ( 30000 );
+
+
+//	  server.accept ( new_sock );
+
+	  try
+	    {
+		  new_sock >> client_data;
+		  new_sock << server_data;
+	    }
+	  catch ( SocketException& ) {}
+
+	
+    }
+  catch ( SocketException& e )
+    {
+      std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
+    }
+	
+//	std::cout << client_data << "\n";
+//	std::cout << server_data << "\n";
+	
+//	server_data = " ";
+	
 	//.................................* end of server *..................................
 				
 		
